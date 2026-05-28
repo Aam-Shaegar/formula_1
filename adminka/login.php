ca08b61b78829cdf436f1b5c23de2a7675872496
Dmitry Solovev <dimalexandrovich2006@gmail.com>
2026-05-28 03:15:46 +0300

    fixed admin6

--- adminka/login.php
+++ adminka/login.php
@@ -1,3 +1,26 @@
 <?php
-echo "PHP works!";
+session_start();
+$error = '';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = trim($_POST['username'] ?? '');
+    $password = trim($_POST['password'] ?? '');
+    
+    echo "Username: $username, Password: $password";
+    exit;
+}
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Admin Login Test</title>
+    <meta charset="UTF-8">
+</head>
+<body>
+    <form method="POST">
+        <input type="text" name="username" placeholder="Username" required>
+        <input type="password" name="password" placeholder="Password" required>
+        <button type="submit">Login</button>
+    </form>
+</body>
+</html>
